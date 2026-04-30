c399372d1c9905eba6f607b40d72ebc957e1f90c
wai-lau <wl.wailau@gmail.com>
2026-04-30 14:23:34 -0400

    style(credits): indent subfolders 2sp, names 4sp

--- nightfall-src/components/GameCredits.tsx
+++ nightfall-src/components/GameCredits.tsx
@@ -5,62 +5,59 @@ import "./GameCredits.css";
 const leftText = `
 Original Gamelab Game (2002)/
 
-_Code/
-P e t er     L ee
+  _Code/
+    P e t er     L ee
 
 
-__Game Design/
-  Nick   For t u gno
-  Fr an k  Lantz
+  __Game Design/
+    Nick   For t u gno
+    Fr an k  Lantz
     Eric     Zim m er man
 
 
-_Graphics/
-  Frank    L ant z
-J u nk o  Ot su k i
+  _Graphics/
+    Frank    L ant z
+    J u nk o  Ot su k i
 
-______Process/
+  ______Process/
     Frank       Lantz
 
 
-__Audio/
-Mi cheal  Swee t _   A u d i  o b   r ai n
+  __Audio/
+    Mi cheal  Swee t _   A u d i  o b   r ai n
 
 
-_QA
-J ess ica            H amm er
+  _QA
+    J ess ica            H amm er
 
 
-_Lego.com/
- Tom a s   Clar k
-V ina y            Tal ley
+  _Lego.com/
+    Tom a s   Clar k
+    V ina y            Tal ley
 `;
 
 const rightText = `
 Remake (2020)/
 
-_Code/
-Pa tr i c k    P an
+  _Code/
+    Pa tr i c k    P an
 
-_Spritesheet/
-S lee py   H arr   y
+  _Spritesheet/
+    S lee py   H arr   y
 
-__Fonts/
-Bi t L i ght  - S ar u
-JH Ti tl e s  - Joi r o  H it ag a ya
-0 4b2 5   - Yu  ji O shi moto
-
-___Let's Play (YouTube)/
-Em p  ire 8  74
+  __Fonts/
+    Bi t L i ght  - S ar u
+    JH Ti tl e s  - Joi r o  H it ag a ya
+    0 4b2 5   - Yu  ji O shi moto
 
 
 Remake+ (2026)/
 
-_QoL + Hosting/
-W ai      L au
+  _QoL + Hosting/
+    W ai      L au
 
-_Music Archivist/
-Jo el   Jak  ub ov ic
+  _Music Archivist/
+    Jo el   Jak  ub ov ic
 `;
 
 const GameCredits = () => (
