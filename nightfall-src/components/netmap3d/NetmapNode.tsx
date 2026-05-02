08651109913f8d4ce6010cceba47ed08a6863aa0
wai-lau <wl.wailau@gmail.com>
2026-05-02 18:06:12 -0400

    fix(nodes): MODEL_SCALE=400

--- nightfall-src/components/netmap3d/NetmapNode.tsx
+++ nightfall-src/components/netmap3d/NetmapNode.tsx
@@ -22,7 +22,7 @@ const GLB_URLS: Record<string, string> = {
   warez: require("../../img/nodes/3d/warez.glb"),
 };
 
-const MODEL_SCALE = 100;
+const MODEL_SCALE = 400;
 
 const COLOR_UNCLEARED = 0x8faabb;
 const COLOR_CLEARED = 0x6a8a9e;
